22e2ae6084515fcb06678391b404807a67c3b5b8
vigork <vigorkfc@gmail.com>
2023-01-18 16:28:06 +0800

    apply dependency-management

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,45 @@
 plugins {
     java
     id("org.springframework.boot") version "2.7.7"
-    id("io.spring.dependency-management") version "1.0.15.RELEASE"
 }
+apply(plugin = "io.spring.dependency-management")
 
 group = "team.iks"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
-configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
-    }
+springBoot {
+    buildInfo()
 }
 
 repositories {
-    maven(url = "https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
+    maven("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
     mavenCentral()
 }
 
 extra["springBootAdminVersion"] = "2.7.4"
 
+the<io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension>().apply {
+    imports {
+        mavenBom("de.codecentric:spring-boot-admin-dependencies:${project.extra["springBootAdminVersion"]}")
+    }
+}
+
 dependencies {
     implementation("org.xerial:sqlite-jdbc:3.40.0.0")
     implementation("com.github.gwenn:sqlite-dialect:0.1.2")
 
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("de.codecentric:spring-boot-admin-starter-client")
     implementation("de.codecentric:spring-boot-admin-starter-server")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.flywaydb:flyway-core")
-    compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-dependencyManagement {
-    imports {
-        mavenBom("de.codecentric:spring-boot-admin-dependencies:${property("springBootAdminVersion")}")
-    }
 }
 
 tasks.withType<Test> {
